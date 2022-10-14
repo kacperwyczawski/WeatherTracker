ea5d4bbe810605a6a4899f764c59d09d611e6b8b
Kacper Wyczawski <kacper@wyczawski.dev>
2022-10-14 11:24:56 +0200

    Add SQLite database

---
+++ WeatherTracker/Data/WeatherTrackerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeatherTracker.Models;
+
+namespace WeatherTracker.Data;
+
+public class WeatherTrackerDbContext : DbContext
+{
+    public WeatherTrackerDbContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    public DbSet<City> UserCities => Set<City>();
+
+    public DbSet<WeatherData> WeatherDataSets => Set<WeatherData>();
+}
---
+++ WeatherTracker/Migrations/20221014092309_InitialCreate.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeatherTracker.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserCities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserCities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WeatherDataSets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Temperature = table.Column<double>(type: "REAL", nullable: false),
+                    Humidity = table.Column<double>(type: "REAL", nullable: false),
+                    CityId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeatherDataSets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WeatherDataSets_UserCities_CityId",
+                        column: x => x.CityId,
+                        principalTable: "UserCities",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WeatherDataSets_CityId",
+                table: "WeatherDataSets",
+                column: "CityId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WeatherDataSets");
+
+            migrationBuilder.DropTable(
+                name: "UserCities");
+        }
+    }
+}
