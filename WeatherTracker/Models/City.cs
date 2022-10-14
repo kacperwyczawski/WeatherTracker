eed9a2bde9ddc0637d9ddf545a2f95958374b73e
Kacper Wyczawski <kacper@wyczawski.dev>
2022-10-14 10:43:48 +0200

    Create models

---
+++ WeatherTracker/Models/City.cs
@@ -0,0 +1,12 @@
+﻿namespace WeatherTracker.Models;
+
+public class City
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public Stack<WeatherData> WeatherData { get; set; }
+
+    public WeatherData NewestWeatherData => WeatherData.Peek();
+}
---
+++ WeatherTracker/Models/WeatherData.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherTracker.Models;
+
+public class WeatherData
+{
+    public DateTime Date { get; set; }
+
+    public double Temperature { get; set; }
+
+    public double Humidity { get; set; }
+}
