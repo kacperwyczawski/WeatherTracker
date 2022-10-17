2472e05ee0e3775f02d278efdea7e4bb944dd935
Kacper Wyczawski <kacper@wyczawski.dev>
2022-10-17 08:27:02 +0200

    Add migration

---
+++ WeatherTracker/Migrations/20221017062618_AddCoordinatesToCity.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeatherTracker.Migrations
+{
+    public partial class AddCoordinatesToCity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Latitude",
+                table: "UserCities",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Longitude",
+                table: "UserCities",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "UserCities");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "UserCities");
+        }
+    }
+}
