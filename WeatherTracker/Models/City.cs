66a9b50fdfbff6c8c7c62e80891c6344deb36a80
Kacper Wyczawski <kacper@wyczawski.dev>
2022-10-17 08:21:46 +0200

    Change Stack to List in City model

--- WeatherTracker/Models/City.cs
+++ WeatherTracker/Models/City.cs
@@ -6,7 +6,7 @@ public class City
 
     public string Name { get; set; }
 
-    public Stack<WeatherData> WeatherData { get; set; }
+    public List<WeatherData> WeatherData { get; set; }
 
-    public WeatherData NewestWeatherData => WeatherData.Peek();
+    public WeatherData NewestWeatherData => WeatherData.Last();
 }
