bbcf5a730010ba072ae3e3d7d86df1fd275b7056
Kacper Wyczawski <wyczawskikacper@gmail.com>
2022-10-17 13:32:37 +0200

    Extract interface for CityService

---
+++ WeatherTracker/Services/ICityService.cs
@@ -0,0 +1,11 @@
+﻿using WeatherTracker.Models;
+
+namespace WeatherTracker.Services;
+
+public interface ICityService
+{
+    Task<City> GetCityAsync(int id);
+    Task<IEnumerable<City>> GetAllCitiesAsync();
+    Task AddCityAsync(City city);
+    Task DeleteCityAsync(int id);
+}
