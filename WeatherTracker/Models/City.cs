6958a317bc0b79b06e4241d62276e00987c36607
Kacper Wyczawski <kacper@wyczawski.dev>
2022-10-17 08:25:16 +0200

    Add Latitude & Longitude to city model

--- WeatherTracker/Models/City.cs
+++ WeatherTracker/Models/City.cs
@@ -6,6 +6,10 @@ public class City
 
     public string Name { get; set; }
 
+    public decimal Latitude { get; set; }
+
+    public decimal Longitude { get; set; }
+
     public List<WeatherData> WeatherData { get; set; }
 
     public WeatherData NewestWeatherData => WeatherData.Last();
