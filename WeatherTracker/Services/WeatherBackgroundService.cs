fffced1d709e414ed3296144c0090068bb313e9b
Kacper Wyczawski <kacper@wyczawski.dev>
2022-10-17 14:49:19 +0200

    Add WeatherBackgroundService for fetching data form api every x minutes and adding it to db

---
+++ WeatherTracker/Services/WeatherBackgroundService.cs
@@ -0,0 +1,81 @@
+﻿using System.Text.Json.Serialization;
+using WeatherTracker.Models;
+
+namespace WeatherTracker.Services;
+
+public class WeatherBackgroundService : BackgroundService
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    private readonly IServiceProvider _serviceProvider;
+    private readonly PeriodicTimer _timer = new(TimeSpan.FromSeconds(5));
+
+    public WeatherBackgroundService(IHttpClientFactory httpClientFactory, IServiceProvider serviceProvider)
+    {
+        _httpClientFactory = httpClientFactory;
+        _serviceProvider = serviceProvider;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        using var scope = _serviceProvider.CreateScope();
+        var cityService = scope.ServiceProvider.GetService<ICityService>();
+
+        while (await _timer.WaitForNextTickAsync(stoppingToken)
+               && !stoppingToken.IsCancellationRequested)
+        {
+            await UpdateWeather(cityService, stoppingToken);
+        }
+    }
+
+    private async Task UpdateWeather(ICityService cityService, CancellationToken stoppingToken)
+    {
+        foreach (var city in await cityService.GetAllCitiesAsync())
+        {
+            Root? weather;
+
+            // get current weather
+            var client = _httpClientFactory.CreateClient();
+
+            var response = await client.GetAsync("https://api.openweathermap.org/data/2.5/weather" +
+                                                 $"?lat={city.Latitude}" +
+                                                 $"&lon={city.Longitude}" +
+                                                 "&appid=73f26086a5140f643f3b25bf3d7a5c29" +
+                                                 "&units=metric", stoppingToken);
+
+            // deserialize
+            if (response.IsSuccessStatusCode)
+                weather = await response.Content.ReadFromJsonAsync<Root>(cancellationToken: stoppingToken);
+            else
+                throw new Exception("Http request failed");
+            
+            // add weather to db
+            var weatherData = new WeatherData
+            {
+                Date = DateTime.Now,
+                Humidity = weather.Main.Humidity,
+                Temperature = weather.Main.Temperature,
+                IconId = weather.Weather[0].Icon
+            };
+
+            await cityService.AddWeatherDataAsync(city.Id, weatherData);
+        }
+    }
+
+    private class Main
+    {
+        [JsonPropertyName("temp")] public double Temperature { get; set; }
+        [JsonPropertyName("humidity")] public int Humidity { get; set; }
+    }
+
+    private class Root
+    {
+        [JsonPropertyName("weather")] public List<Weather> Weather { get; set; }
+        [JsonPropertyName("main")] public Main Main { get; set; }
+    }
+
+    private class Weather
+    {
+        [JsonPropertyName("icon")] public string Icon { get; set; }
+    }
+}
