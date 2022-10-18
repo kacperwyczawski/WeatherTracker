a50bc6a672838dbec733b420fb9415fb938d2e41
Kacper Wyczawski <kacper@wyczawski.dev>
2022-10-18 08:11:30 +0200

    Add migration "AddCityIdToWeatherData"

--- WeatherTracker/Migrations/20221018055850_AddCityIdToWeatherData.Designer.cs
+++ WeatherTracker/Migrations/20221018055850_AddCityIdToWeatherData.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WeatherTracker.Data;
 
@@ -10,9 +11,10 @@ using WeatherTracker.Data;
 namespace WeatherTracker.Migrations
 {
     [DbContext(typeof(WeatherTrackerDbContext))]
-    partial class WeatherTrackerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221018055850_AddCityIdToWeatherData")]
+    partial class AddCityIdToWeatherData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.10");
---
+++ WeatherTracker/Migrations/20221018055850_AddCityIdToWeatherData.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeatherTracker.Migrations
+{
+    public partial class AddCityIdToWeatherData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WeatherDataSets_UserCities_CityId",
+                table: "WeatherDataSets");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Temperature",
+                table: "WeatherDataSets",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "REAL");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Humidity",
+                table: "WeatherDataSets",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "REAL");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CityId",
+                table: "WeatherDataSets",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WeatherDataSets_UserCities_CityId",
+                table: "WeatherDataSets",
+                column: "CityId",
+                principalTable: "UserCities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WeatherDataSets_UserCities_CityId",
+                table: "WeatherDataSets");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Temperature",
+                table: "WeatherDataSets",
+                type: "REAL",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Humidity",
+                table: "WeatherDataSets",
+                type: "REAL",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CityId",
+                table: "WeatherDataSets",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WeatherDataSets_UserCities_CityId",
+                table: "WeatherDataSets",
+                column: "CityId",
+                principalTable: "UserCities",
+                principalColumn: "Id");
+        }
+    }
+}
